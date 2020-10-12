c7bf41ed0a68f8addf1e6f0643143fd8c36c2a0c
solomia2003 <ssolomiashep@gmai.com>
2020-10-12 12:54:03 +0300

    соломія

---
+++ лаб_2.1/лаб_2.1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>;
+#include <cmath>;
+
+using namespace std;
+
+int main()
+{
+	double z1;
+	double z2;
+	double x;
+
+	cout << "x = "; cin >> x;
+
+	z1 = (sin(2 * x) + sin(5 * x) - sin(3 * x)) / (cos(x) + 1 - 2 * pow(sin(2 * x), 2));
+	z2 = (2 * sin(x));
+	cout << endl;     
+	cout << "z1 = " << z1 << endl;     
+	cout << "z2 = " << z2 << endl;
+
+	cin.get();     
+	return 0;
+}
